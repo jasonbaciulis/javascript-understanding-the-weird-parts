5748693965ff152d9d4ee2e9be0d998f9a45faaf
Ainhoa Rebolledo Torrens <rebolledotorrens@gmail.com>
2020-05-10 12:31:34 +0200

    sunday

--- Section4_Objects/D3_FakingNamespaces/Starter/app.js
+++ Section4_Objects/D3_FakingNamespaces/Starter/app.js
@@ -2,3 +2,12 @@ var greet = 'Hello!';
 var greet = 'Hola!'; 
 
 console.log(greet);
+
+var english = {};
+var spanish = {};
+
+
+english.greet = 'Hello!';
+spanish.greet = 'Hola';
+
+console.log(english);
