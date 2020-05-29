4834438320fb9aba1e3feac66c61657b042fe8dd
Ainhoa Torrens <ainhoa.torrens@intranet.ametsis.com>
2020-05-29 14:50:35 +0200

    friday

--- Section4_Objects/D20b_FunctionalProgrammingPart2/Starter/app.js
+++ Section4_Objects/D20b_FunctionalProgrammingPart2/Starter/app.js
@@ -45,29 +45,9 @@ console.log(arr5);
 
 
 
+// underscore
+var arr6 = _.map(arr1, function(item) { return item * 3 });
+console.log(arr6);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var arr7 = _.filter([2,3,4,5,6,7], function(item) { return item % 2 === 0; });
+console.log(arr7); 
---
+++ Section5/E2_UnderstandingThePrototype/Starter/app.js
@@ -0,0 +1,27 @@
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function() {
+        return this.firstname + ' ' + this.lastname;
+    }
+}
+
+var john = {
+    firstname: 'John',
+    lastname: 'Doe'
+}
+
+// dont do this
+
+john.__proto__ = person;
+
+console.log(john.getFullName());
+console.log(john.firstname);
+
+var jane = {
+    firstname: 'Jane'
+}
+
+jane.__proto__ = person;
+
+console.log(jane.getFullName());
