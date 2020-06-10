73532508b56678e5c6da101b24e5c3e64bf4199f
Ainhoa Torrens <rebolledotorrens@gmail.com>
2020-06-10 15:06:53 +0200

    wednesday

--- Section6/F5_ObjectCreateAndPrototypal/Starter/app.js
+++ Section6/F5_ObjectCreateAndPrototypal/Starter/app.js
@@ -1,3 +1,15 @@
+if (!Object.create) {
+    Object.create = function (o) {
+      if (arguments.length > 1) {
+        throw new Error('Object.create implementation'
+        + ' only accepts the first parameter.');
+      }
+      function F() {}
+      F.prototype = o;
+      return new F();
+    };
+  }
+
 var person = {
     firstname: 'Default',
     lastname: 'Default',
@@ -7,4 +19,6 @@ var person = {
 };
 
 var john = Object.create(person);
+john.firstname = 'John';
+john.lastname = 'Doe';
 console.log(john);
